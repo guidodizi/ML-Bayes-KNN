3ae762387a372ded785ebcd3a1c8796a8e38b0cc
Guido Dizioli <guidodizi@hotmail.com>
2017-04-19 18:30:37 -0300

    restableciendo de Practico 2 a Practico 3

--- parser_csv.py
+++ parser_csv.py
@@ -1,30 +1,16 @@
 import csv
-from utils import Line 
+from utils import * 
 import string
 import sys
 from operator import is_not
 from functools import partial
-from id3 import *
 
-FILE_NAME = 'bank-full.csv'
+FILE_NAME = 'bank-additional-full.csv'
 #FILE_NAME = 'bank.csv'
 
-class Common_Values:
-	
-	def __init__(self):
-		self.jobs = [0] * len(VALUES_JOB)
-		self.marital = [0] * len(VALUES_MARITAL)
-		self.education = [0] * len(VALUES_EDUCATION)
-		self.default = [0] * len(VALUES_DEFAULT)
-		self.housing = [0] * len(VALUES_HOUSING)
-		self.loan = [0] * len(VALUES_LOAN)
-		self.contact = [0] * len(VALUES_CONTACT)
-		self.poutcome = [0] * len(VALUES_POUTCOME)
-		
-
-def isInt(s):
+def isFloat(s):
     try: 
-        int(s)
+        float(s)
         return True
     except ValueError:
         return False
@@ -65,11 +51,10 @@ def makeIntervals(min,max,qty):
 		i+=1
 		intervals.append(intervals_med)
 	return intervals
-	
+
 def createIntervalsAttributes():
 	first = True
 	second = True
-	common_values = Common_Values()
 	attributes = []
 	numericAtt = []
 	with open(FILE_NAME, 'rb') as csvfile:
@@ -79,7 +64,9 @@ def createIntervalsAttributes():
 				c = line.split(';')
 				for a in c:
 					attribute = str.replace(a,'"','')
-					attribute = str.replace(attribute,'\n','')				
+					attribute = str.replace(attribute,'\n','')	
+					attribute = str.replace(attribute,'\r','')												
+					attribute = str.replace(attribute,'.','_')												
 					attributes.append(attribute)
 				size = len(attributes)
 				min = [None] * size
@@ -90,55 +77,23 @@ def createIntervalsAttributes():
 				for a in c:
 					attribute = str.replace(a,'"','')
 					attribute = str.replace(attribute,'\n','')
-					if i == 1:
-					#attribute jobs
-						ind = VALUES_JOB.index(attribute)
-						common_values.jobs[ind] += 1
-					elif i == 2:
-					#attribute marital						
-						ind = VALUES_MARITAL.index(attribute)
-						common_values.marital[ind] += 1
-					elif i == 3:
-					#attribute education
-						ind = VALUES_EDUCATION.index(attribute)
-						common_values.education[ind] += 1
-					elif i == 4:
-					#attribute default
-						ind = VALUES_DEFAULT.index(attribute)
-						common_values.default[ind] += 1
-					elif i == 6:
-					#attribute housing
-						ind = VALUES_HOUSING.index(attribute)
-						common_values.housing[ind] += 1
-					elif i == 7:
-					#attribute loan
-						ind = VALUES_LOAN.index(attribute)
-						common_values.loan[ind] += 1
-					elif i == 8:
-					#attribute contact
-						ind = VALUES_CONTACT.index(attribute)
-						common_values.contact[ind] += 1
-					elif i == 15:
-					#attribute poutcome
-						ind = VALUES_POUTCOME.index(attribute)
-						common_values.poutcome[ind] += 1
-
-					if isInt(attribute):
+					attribute = str.replace(attribute,'\r','')					
+					if isFloat(attribute):
 						if second:
 							numericAtt.append(attributes[i])
-						if min[i] == None or int(attribute) < min[i]:						
-							min[i]= int(attribute)					
-						elif max[i] == None or int(attribute) > max[i]:
-							max[i]= int(attribute)
+						if min[i] == None or float(attribute) < min[i]:						
+							min[i]= float(attribute)					
+						elif max[i] == None or float(attribute) > max[i]:
+							max[i]= float(attribute)
 					i+=1
 				second = False
 		min = filter(partial(is_not, None), min)
 		max = filter(partial(is_not, None), max)
 		intervals = makeIntervals(min,max,4)
 		
-	return intervals,attributes,common_values
+	return intervals,attributes
 
-def getData(intervals,attributes, common_values, fix_unknown = False):
+def getData(intervals,attributes):
 	first = True
 	items = []
 	with open(FILE_NAME, 'rb') as csvfile:
@@ -152,55 +107,39 @@ def getData(intervals,attributes, common_values, fix_unknown = False):
 				i = 0
 				for a in c:
 					attribute_value = str.replace(a,'"','')
-					attribute_value = str.replace(attribute_value,'\n','')	
-					if (fix_unknown) and (attribute_value == "unknown"):
-						if i == 1:
-						#attribute jobs
-							attribute_value = VALUES_JOB[common_values.jobs.index(max(common_values.jobs))]
-						elif i == 2:
-						#attribute marital						
-							attribute_value = VALUES_MARITAL[common_values.marital.index(max(common_values.marital))]
-						elif i == 3:
-						#attribute education
-							attribute_value = VALUES_EDUCATION[common_values.education.index(max(common_values.education))]
-						elif i == 4:
-						#attribute default
-							attribute_value = VALUES_DEFAULT[common_values.default.index(max(common_values.default))]
-						elif i == 6:
-						#attribute housing
-							attribute_value = VALUES_HOUSING[common_values.housing.index(max(common_values.housing))]
-						elif i == 7:
-						#attribute loan
-							attribute_value = VALUES_LOAN[common_values.loan.index(max(common_values.loan))]
-						elif i == 8:
-						#attribute contact
-							attribute_value = VALUES_CONTACT[common_values.contact.index(max(common_values.contact))]
-						elif i == 15:
-						#attribute poutcome
-							attribute_value = VALUES_POUTCOME[common_values.poutcome.index(max(common_values.poutcome))]
-
-					if isInt(attribute_value):
+					attribute_value = str.replace(attribute_value,'\n','')
+					attribute_value = str.replace(attribute_value,'\r','')						
+					if isFloat(attribute_value):
 						if i == 0:#es age
 							interval = intervals[0]
 							values = VALUES_AGE
-						elif i == 1:#es balance
+						elif i == 1:#es duration
 							interval = intervals[1]
-							values = VALUES_BALANCE
-						elif i == 2:#es day
-							interval = intervals[2]
-							values = VALUES_DAY
-						elif i == 3:#es duration
-							interval = intervals[3]
 							values = VALUES_DURATION
-						elif i == 4:#es campaign
-							interval = intervals[4]
+						elif i == 2:#es campaign
+							interval = intervals[2]
 							values = VALUES_CAMPAIGN
-						elif i == 5:#es paydays
-							interval = intervals[5]
+						elif i == 3:#es paydays
+							interval = intervals[3]
 							values = VALUES_PDAYS
-						elif i == 6:#es campaign
+						elif i == 4:#es previous
+							interval = intervals[4]
+							values = VALUES_PREVIOUS
+						elif i == 5:#es emp_var_rate
+							interval = intervals[5]
+							values = VALUES_PREVIOUS
+						elif i == 6:#es cons_price_idx
 							interval = intervals[6]
-							values = VALUES_PREVIUOS
+							values = VALUES_PREVIOUS
+						elif i == 7:#es cons_conf_idx
+							interval = intervals[7]
+							values = VALUES_PREVIOUS
+						elif i == 8:#es euribor3m
+							interval = intervals[8]
+							values = VALUES_PREVIOUS
+						elif i == 9:#es nr_employed
+							interval = intervals[9]
+							values = VALUES_PREVIOUS
 										
 						if float(attribute_value) < interval[0][0]:
 							attribute_value = values[0]
@@ -213,9 +152,7 @@ def getData(intervals,attributes, common_values, fix_unknown = False):
 						i += 1
 						
 					l.append(attribute_value)
-					
-				item = Line(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9],l[10],l[11],l[12],l[13],l[14],l[15],l[16])
+				item = Line(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9],l[10],l[11],l[12],l[13],l[14],l[15],l[16],l[17],l[18],l[19],l[20])
 				items.append(item)
 	return items
 			
-	
--- utils.py
+++ utils.py
@@ -1,98 +1,74 @@
+# age (numeric)
+# job : type of job (categorical: 'admin.','blue-collar','entrepreneur','housemaid','management','retired','self-employed','services','student','technician','unemployed','unknown')
+# marital : marital status (categorical: 'divorced','married','single','unknown'; note: 'divorced' means divorced or widowed)
+# education (categorical: 'basic.4y','basic.6y','basic.9y','high.school','illiterate','professional.course','university.degree','unknown')
+# default: has credit in default? (categorical: 'no','yes','unknown')
+# housing: has housing loan? (categorical: 'no','yes','unknown')
+# loan: has personal loan? (categorical: 'no','yes','unknown')
+# # related with the last contact of the current campaign:
+# contact: contact communication type (categorical: 'cellular','telephone') 
+# month: last contact month of year (categorical: 'jan', 'feb', 'mar', ..., 'nov', 'dec')
+# day_of_week: last contact day of the week (categorical: 'mon','tue','wed','thu','fri')
+# duration: last contact duration, in seconds (numeric). Important note: this attribute highly affects the output target (e.g., if duration=0 then y='no'). Yet, the duration is not known before a call is performed. Also, after the end of the call y is obviously known. Thus, this input should only be included for benchmark purposes and should be discarded if the intention is to have a realistic predictive model.
+# # other attributes:
+# campaign: number of contacts performed during this campaign and for this client (numeric, includes last contact)
+# pdays: number of days that passed by after the client was last contacted from a previous campaign (numeric; 999 means client was not previously contacted)
+# previous: number of contacts performed before this campaign and for this client (numeric)
+# poutcome: outcome of the previous marketing campaign (categorical: 'failure','nonexistent','success')
+# # social and economic context attributes
+# emp_var_rate: employment variation rate - quarterly indicator (numeric)
+# cons_price_idx: consumer price index - monthly indicator (numeric) 
+# cons_conf_idx: consumer confidence index - monthly indicator (numeric) 
+# euribor3m: euribor 3 month rate - daily indicator (numeric)
+# nr_employed: number of employees - quarterly indicator (numeric)
+# #Output variable (desired target):
+# 21 - y - has the client subscribed a term deposit? (binary: 'yes','no')
+
 #CUANDO SON VALORES NUMERICOS HACEMOS UN PROCESAMIENTO DE LOS DATOS PARA CREAR INTERVALOS, Y LUEGO AL CREAR EL OBJETO LINE
 #LO ASIGNAMOS DENTRO DE UNO DE LOS VALORES YA EXISTENTES
 VALUES_AGE = ["teen","adult","grown","elder"]
 VALUES_JOB =  ["admin.","blue-collar","entrepreneur","housemaid","management","retired","self-employed","services","student","technician","unemployed","unknown"]
 VALUES_MARITAL = ["divorced","married","single","unknown"]
-#VALUES_EDUCATION = ["basic.4y","basic.6y","basic.9y","high.school","illiterate","professional.course","university.degree","unknown"]
-VALUES_EDUCATION = ["primary","secondary","tertiary","unknown"]
+VALUES_EDUCATION = ["basic.4y","basic.6y","basic.9y","high.school","illiterate","professional.course","university.degree","unknown"]
 VALUES_DEFAULT = ["no","yes","unknown"]
-VALUES_BALANCE = ["low", "mid-low", "mid-high","high"]
 VALUES_HOUSING = ["no","yes","unknown"]
 VALUES_LOAN = ["no","yes","unknown"]
-VALUES_CONTACT = ["cellular","telephone","unknown"]
-VALUES_DAY = ["start","mid-start","mid-end","end"]
+VALUES_CONTACT = ["cellular","telephone"]
 VALUES_MONTH = ["jan", "feb", "mar", "apr", "may","jun","jul","aug","sep","oct","nov", "dec"]
+VALUES_DAY_OF_WEEK = ["mon","tue","wed","thu","fri"]
 VALUES_DURATION = ["short", "mid-short", "mid-long","long"]
 VALUES_CAMPAIGN = ["low", "mid-low", "mid-high","high"]
 VALUES_PDAYS = ["low", "mid-low", "mid-high","high"]
-VALUES_PREVIUOS = ["low", "mid-low", "mid-high","high"]
-VALUES_POUTCOME = ["failure","other","success","unknown"]
+VALUES_PREVIOUS = ["low", "mid-low", "mid-high","high"]
+VALUES_POUTCOME = ["failure","nonexistent","success"]
+VALUES_EMP_VAR_RATE = ["low", "mid-low", "mid-high","high"]
+VALUES_CONS_PRICE_IDX = ["low", "mid-low", "mid-high","high"]
+VALUES_CONS_CONF_IDX = ["low", "mid-low", "mid-high","high"]
+VALUES_EURIBOR3M = ["low", "mid-low", "mid-high","high"]
+VALUES_NR_EMPLOYED = ["low", "mid-low", "mid-high","high"]
 
-#clase usada para la generacion del arbol de decision
-class Node:
-   
-	def __init__(self):
-		self.info = ""
-		self.value_from = ""
-		self.children = []
-
-	def printNode(self):
-
-		print "node info: " + self.info
-		print "node value: " + self.value_from
-		hijo = 1
-		for c in self.children:
-			print "hijo: "  + str(hijo) + " de " + str(self.info)
-			c.printNode()
-			hijo +=1
-			
-	def setInfo(self,info):
-		self.info = info
-
-	def setValueForm(self,value_from):
-		self.value_from = value_from
-		
-	def setChild(self,child):
-		self.children.append(child)
-		
-	def eval(self, line):
-		if (self.children == []) and ((self.info == "yes") or (self.info == "no")):
-			return (line.y == self.info)
-		else:
-			for child in self.children:
-				lineAtributeValue = getattr(line, self.info)
-				if (child.value_from == lineAtributeValue):
-					return (child.eval(line))
-				
 class Line:
-
-	def __init__(self,age1,job,marital,education,default,balance,housing,loan,contact,day,month,duration,campaign,pdays,previous,poutcome,y):
-		self.age = age1
+	def __init__(self,age,job,marital,education,default,housing,loan,contact,month,day_of_week,duration,
+	campaign,pdays,previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y):
+		self.age = age
 		self.job = job
-		self.marital   = marital
+		self.marital = marital
 		self.education = education
-		self.default   = default
-		self.balance   = balance
-		self.housing   = housing
-		self.loan      = loan
-		self.contact   = contact
-		self.day       = day
-		self.month     = month
-		self.duration  = duration
-		self.campaign  = campaign
-		self.pdays     = pdays
-		self.previous  = previous
-		self.poutcome  = poutcome
-		self.y         = y
-	
-	def printLine(self):
-		
-		print "Age:  " + self.age
-		print "Job: " + self.job
-		print "Marital: " + self.marital
-		print "Education: " + self.education
-		print "Default: " + self.default
-		print "Balance: " + self.balance
-		print "Housing: " + self.housing
-		print "Loan: " + self.loan
-		print "Contact: " + self.contact
-		print "Day: " + self.day
-		print "Month: " + self.month
-		print "Duration: " + self.duration
-		print "Campaign: " + self.campaign
-		print "Pdays: " + self.pdays
-		print "Previous: " + self.previous
-		print "Poutcome: " + self.poutcome
-		print "Y: " + self.y
-		print "--------------------"	
+		self.default = default
+		self.housing = housing
+		self.loan = loan
+		self.contact = contact
+		self.month = month
+		self.day_of_week = day_of_week
+		self.duration = duration
+		self.campaign = campaign
+		self.pdays = pdays
+		self.previous = previous
+		self.poutcome = poutcome
+		self.emp_var_rate = emp_var_rate
+		self.cons_price_idx = cons_conf_idx
+		self.cons_conf_idx = cons_conf_idx
+		self.euribor3m = euribor3m
+		self.nr_employed = nr_employed
+		self.y = y
 	
