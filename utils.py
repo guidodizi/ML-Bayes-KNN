38869b5459e5abb0c93c62fdff111af106befe36
Guido Dizioli <guidodizi@hotmail.com>
2017-04-20 09:32:49 -0300

    bayesian learning

--- utils.py
+++ utils.py
@@ -73,10 +73,140 @@ class Line:
 		self.y = y
 
 class Struct:
-	def __init__(self, numYes, numNo, p_yes, p_no):
-		self.numYes = numYes
-		self.numNo = numNo
-		self.p_yes = p_yes
-		self.p_no = p_no
+	def __init__(self, train_data):
+		self.train_data = train_data
+		self.numYes = 0
+		self.numNo = 0
+		#dictionaries with all values of attribute and a tuple with numbers of yes and no for that value
+		self.dict_age = {}
+		for value in VALUES_AGE:
+			self.dict_age.setdefault(value, [0,0])
 
-		
+		self.dict_job = {}
+		for value in VALUES_JOB:
+			self.dict_job.setdefault(value, [0,0])
+
+		self.dict_marital = {}
+		for value in VALUES_MARITAL:
+			self.dict_marital.setdefault(value, [0,0])
+
+		self.dict_education = {}
+		for value in VALUES_EDUCATION:
+			self.dict_education.setdefault(value, [0,0])
+
+		self.dict_default = {}
+		for value in VALUES_DEFAULT:
+			self.dict_default.setdefault(value, [0,0])
+
+		self.dict_housing = {}
+		for value in VALUES_HOUSING:
+			self.dict_housing.setdefault(value, [0,0])
+
+		self.dict_loan = {}
+		for value in VALUES_LOAN:
+			self.dict_loan.setdefault(value, [0,0])
+
+		self.dict_contact = {}
+		for value in VALUES_CONTACT:
+			self.dict_contact.setdefault(value, [0,0])
+
+		self.dict_month = {}
+		for value in VALUES_MONTH:
+			self.dict_month.setdefault(value, [0,0])
+
+		self.dict_day_of_week  = {}
+		for value in VALUES_DAY_OF_WEEK:
+			self.dict_day_of_week.setdefault(value, [0,0])
+
+		self.dict_duration  = {}
+		for value in VALUES_DURATION:
+			self.dict_duration.setdefault(value, [0,0])
+
+		self.dict_campaign  = {}
+		for value in VALUES_CAMPAIGN:
+			self.dict_campaign.setdefault(value, [0,0])
+
+		self.dict_pdays = {}
+		for value in VALUES_PDAYS:
+			self.dict_pdays.setdefault(value, [0,0])
+
+		self.dict_previous = {}
+		for value in VALUES_PREVIOUS:
+			self.dict_previous.setdefault(value, [0,0])
+
+		self.dict_poutcome = {}
+		for value in VALUES_POUTCOME:
+			self.dict_poutcome.setdefault(value, [0,0])
+
+		self.dict_emp_var_rate = {}
+		for value in VALUES_EMP_VAR_RATE:
+			self.dict_emp_var_rate.setdefault(value, [0,0])
+
+		self.dict_cons_price_idx = {}
+		for value in VALUES_CONS_PRICE_IDX:
+			self.dict_cons_price_idx.setdefault(value, [0,0])
+
+		self.dict_cons_conf_idx = {}
+		for value in VALUES_CONS_CONF_IDX:
+			self.dict_cons_conf_idx.setdefault(value, [0,0])
+
+		self.dict_euribor3m = {}
+		for value in VALUES_EURIBOR3M:
+			self.dict_euribor3m.setdefault(value, [0,0])
+
+		self.dict_nr_employed = {}
+		for value in VALUES_NR_EMPLOYED:
+			self.dict_nr_employed.setdefault(value, [0,0])
+
+	def processData(self):
+		for item in self.train_data:
+			if item.y == "yes":
+				#add to age dictonary, on key item.age, to the place 0 of the tuple, which corresponds to YES
+				self.dict_age[item.age][0] += 1
+				self.dict_job[item.job][0] += 1
+				self.dict_marital[item.marital][0] += 1
+				self.dict_education[item.education][0] += 1
+				self.dict_default[item.default][0] += 1
+				self.dict_housing[item.housing][0] += 1
+				self.dict_loan[item.loan][0] += 1
+				self.dict_contact[item.contact][0] += 1
+				self.dict_month[item.month][0] += 1
+				self.dict_day_of_week[item.day_of_week][0] += 1
+				self.dict_duration[item.duration][0] += 1
+				self.dict_campaign[item.campaign][0] += 1
+				self.dict_pdays[item.pdays][0] += 1
+				self.dict_previous[item.previous][0] += 1
+				self.dict_poutcome[item.poutcome][0] += 1
+				self.dict_emp_var_rate[item.emp_var_rate][0] += 1
+				self.dict_cons_price_idx[item.cons_price_idx][0] += 1
+				self.dict_cons_conf_idx[item.cons_conf_idx][0] += 1
+				self.dict_euribor3m[item.euribor3m][0] += 1
+				self.dict_nr_employed[item.nr_employed][0] += 1
+
+				self.numYes += 1
+			elif item.y == "no":
+				#add to age dictonary, on key item.age, to the place 1 of the tuple, which corresponds to NO	
+				self.dict_age[item.age][1] += 1
+				self.dict_job[item.job][1] += 1
+				self.dict_marital[item.marital][1] += 1
+				self.dict_education[item.education][1] += 1
+				self.dict_default[item.default][1] += 1
+				self.dict_housing[item.housing][1] += 1
+				self.dict_loan[item.loan][1] += 1
+				self.dict_contact[item.contact][1] += 1
+				self.dict_month[item.month][1] += 1
+				self.dict_day_of_week[item.day_of_week][1] += 1
+				self.dict_duration[item.duration][1] += 1
+				self.dict_campaign[item.campaign][1] += 1
+				self.dict_pdays[item.pdays][1] += 1
+				self.dict_previous[item.previous][1] += 1
+				self.dict_poutcome[item.poutcome][1] += 1
+				self.dict_emp_var_rate[item.emp_var_rate][1] += 1
+				self.dict_cons_price_idx[item.cons_price_idx][1] += 1
+				self.dict_cons_conf_idx[item.cons_conf_idx][1] += 1
+				self.dict_euribor3m[item.euribor3m][1] += 1
+				self.dict_nr_employed[item.nr_employed][1] += 1
+
+				self.numNo += 1
+		self.p_Yes = float(self.numYes / (self.numYes + self.numNo))
+		self.p_No = float(self.numNo / (self.numYes + self.numNo))
