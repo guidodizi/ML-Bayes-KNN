261197ef71f6da0947e082172bc670833b93ca56
Nicolas <nicolascrocco@gmail.com>
2017-04-23 18:15:44 -0300

    cruzamiento y agregar unknon

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from parser_csv import *
 from random import shuffle
 import time
 #primero obtengo los intervalos y atributos
-intervals, attributes = createIntervalsAttributes()
+intervals, attributes, common_values = createIntervalsAttributes()
+#eligo de que forma utilizar los unknown
+not_ok = True
+mode = input("Ingrese 0 para usar los 'unknown' como un posible valor mas del atributo\nIngrese 1 para preprocesar los 'unknown': ") 
+while not_ok:
+	if mode == 0:
+		fix_unknown = False
+		not_ok = False
+	elif mode == 1:
+		fix_unknown = True
+		not_ok = False
+	else:
+		print "\nValor invalido\n"
+		mode = input("Ingrese 0 para usar los 'unknown' como un posible valor mas del atributo\nIngrese 1 para preprocesar los 'unknown': ") 
+
 #me creo el data
-data = getData(intervals,attributes)
+data = getData(intervals,attributes, common_values, fix_unknown)
 #hago un shuffle del corpus
 shuffle(data)
 #divido el corpus en 4/5 y 1/5
@@ -15,69 +29,164 @@ test_data = data[:index]
 #4/5
 train_data = data[index:] 
 
-index_testing = 10
-testing_data = test_data
-print len(testing_data)
-#class with all data for bayesian learning
-bayesian = BayesianStruct(train_data)
 
-bayesian.processData()
+#PARTE B
+#validacion cruzada, divido mi train data en 10 subconjuntos
+size = int(len(train_data) / 10)
+print "PARTE B\n"
 
-bayesian.evalData(testing_data)
-
-correctPredictionsBayesian = 0
-for item in testing_data:
-	#print "GUESS: " + item.probBayesian
-	#print "TRUTH: " + item.y
-	if item.probBayesian == item.y:
-		correctPredictionsBayesian += 1
+totalCorrectPredictionsBayesian = 0
 print "RESULTS BAYESIAN LEARNING:"
-print "Total cases: " + str(len(testing_data))
-print "Correct Predictions: " + str(correctPredictionsBayesian)
-print "Correctness percentage: " + str((correctPredictionsBayesian / len(testing_data)) * 100) + "%"
+for i in range(0,10):
 
-start2 = time.clock() 
+	#entreno con el train_data - el subocojunto 
+	new_data = train_data[:i*size] + train_data[(i*size)+size:]
+	#class with all data for bayesian learning
+	bayesian = BayesianStruct(new_data)
+	bayesian.processData()
+	#evaluo con el subconjunto 
+	testing_data = train_data[i*size:(i*size)+size]
+	bayesian.evalData(testing_data)
 
-knn = KNN(5, train_data)
-knn.evalData(testing_data)
+	#evaluo con el subocojunto de train_data
+	#PARA EL SUBCONJUNTO
+	print "PARA EL SUBCOJUNTO QUE VA DE: " + str(i*size) + " a " + str((i*size)+size) 
+	correctPredictionsBayesian = 0
+	for item in testing_data:
+		if item.probBayesian == item.y:
+			correctPredictionsBayesian += 1
+			totalCorrectPredictionsBayesian += 1
+	print "Total cases: " + str(len(testing_data))
+	print "Correct Predictions: " + str(correctPredictionsBayesian)
+	print "Correctness percentage: " + str((correctPredictionsBayesian / len(testing_data)) * 100) + "%"
+	print "------------------------------------------"
 
-elapsed2 = time.clock()
-elapsed2 = elapsed2 - start2
+print "\nRESULTADO TOTAL CRUZAMIENTO DE TAMANIO 10: \n"
+print "Total cases: " + str(len(train_data))
+print "Correct Predictions: " + str(totalCorrectPredictionsBayesian)
+print "Correctness percentage: " + str((totalCorrectPredictionsBayesian / len(train_data)) * 100) + "%"
+print "------------------------------------------\n"
 
-correctPredictionsKNN1 = 0
-for item in testing_data:
-	#print "GUESS: " + item.probKNN1
-	#print "TRUTH: " + item.y
-	if item.probKNN1 == item.y:
-		correctPredictionsKNN1 += 1
+totalCorrectPredictionsKNN1 = 0
+totalCorrectPredictionsKNN3 = 0
+totalCorrectPredictionsKNN5 = 0
+print "RESULTS KNN LEARNING:\n"
+for i in range(0,10):
 
-print "RESULTS KNN - 1:"
-print "Total cases: " + str(len(testing_data))
-print "Correct Predictions: " + str(correctPredictionsKNN1)
-print "Correctness percentage: " + str((correctPredictionsKNN1 / len(testing_data)) * 100) + "%"
+	#entreno con el train_data - el subocojunto 
+	new_data = train_data[:i*size] + train_data[(i*size)+size:]
+	start2 = time.clock() 
 
-correctPredictionsKNN3 = 0
-for item in testing_data:
-	#print "GUESS: " + item.probKNN3
-	#print "TRUTH: " + item.y
-	if item.probKNN3 == item.y:
-		correctPredictionsKNN3 += 1
+	knn = KNN(5, new_data)
+	testing_data = train_data[i*size:(i*size)+size]
+	knn.evalData(testing_data)	
+	elapsed2 = time.clock()
+	elapsed2 = elapsed2 - start2
+	#evaluo con el subocojunto de train_data
+	#PARA EL SUBCONJUNTO
+	print "PARA EL SUBCOJUNTO QUE VA DE: " + str(i*size) + " a " + str((i*size)+size) 
+	correctPredictionsKNN1 = 0
+	correctPredictionsKNN3 = 0
+	correctPredictionsKNN5 = 0
+	for item in testing_data:
+		if item.probKNN1 == item.y:
+			correctPredictionsKNN1 += 1
+			totalCorrectPredictionsKNN1 += 1
+		if item.probKNN3 == item.y:
+			correctPredictionsKNN3 += 1
+			totalCorrectPredictionsKNN3 += 1
+		if item.probKNN5 == item.y:
+			correctPredictionsKNN5 += 1
+			totalCorrectPredictionsKNN5 += 1
+	print "Total cases: " + str(len(testing_data))
+	print "For KNN1:"
+	print "Correct Predictions: " + str(correctPredictionsKNN1)
+	print "Correctness percentage: " + str((correctPredictionsKNN1 / len(testing_data)) * 100) + "%"
+	print "For KNN3:"
+	print "Correct Predictions: " + str(correctPredictionsKNN3)
+	print "Correctness percentage: " + str((correctPredictionsKNN3 / len(testing_data)) * 100) + "%"
+	print "For KNN5:"
+	print "Correct Predictions: " + str(correctPredictionsKNN5)
+	print "Correctness percentage: " + str((correctPredictionsKNN5 / len(testing_data)) * 100) + "%"
+	print "Time spent in knn is: ", elapsed2
+	print "------------------------------------------"
 
-print "RESULTS KNN - 3:"
-print "Total cases: " + str(len(testing_data))
-print "Correct Predictions: " + str(correctPredictionsKNN3)
-print "Correctness percentage: " + str((correctPredictionsKNN3 / len(testing_data)) * 100) + "%"
+print "\nRESULTADO TOTAL CRUZAMIENTO DE TAMANIO 10: \n"
+print "Total cases: " + str(len(train_data))
+print "For KNN1:"
+print "Correct Predictions: " + str(totalCorrectPredictionsKNN1)
+print "Correctness percentage: " + str((totalCorrectPredictionsKNN1 / len(train_data)) * 100) + "%"
+print "For KNN3:"
+print "Correct Predictions: " + str(totalCorrectPredictionsKNN3)
+print "Correctness percentage: " + str((totalCorrectPredictionsKNN3 / len(train_data)) * 100) + "%"
+print "For KNN5:"
+print "Correct Predictions: " + str(totalCorrectPredictionsKNN5)
+print "Correctness percentage: " + str((totalCorrectPredictionsKNN5 / len(train_data)) * 100) + "%"
+print "------------------------------------------"
 
-correctPredictionsKNN5 = 0
-for item in testing_data:
-	#print "GUESS: " + item.probKNN5
-	#print "TRUTH: " + item.y
-	if item.probKNN5 == item.y:
-		correctPredictionsKNN5 += 1
+## PARTE C!!!!!
+# index_testing = 10
+# testing_data = test_data
+# print len(testing_data)
+# bayesian = BayesianStruct(train_data)
 
-print "RESULTS KNN - 5:"
-print "Total cases: " + str(len(testing_data))
-print "Correct Predictions: " + str(correctPredictionsKNN5)
-print "Correctness percentage: " + str((correctPredictionsKNN5 / len(testing_data)) * 100) + "%"
+# bayesian.processData()
 
-print "Time spent in knn is: ", elapsed2
+# bayesian.evalData(testing_data)
+
+# correctPredictionsBayesian = 0
+# for item in testing_data:
+# 	#print "GUESS: " + item.probBayesian
+# 	#print "TRUTH: " + item.y
+# 	if item.probBayesian == item.y:
+# 		correctPredictionsBayesian += 1
+# print "RESULTS BAYESIAN LEARNING:"
+# print "Total cases: " + str(len(testing_data))
+# print "Correct Predictions: " + str(correctPredictionsBayesian)
+# print "Correctness percentage: " + str((correctPredictionsBayesian / len(testing_data)) * 100) + "%"
+
+# start2 = time.clock() 
+
+# knn = KNN(5, train_data)
+# knn.evalData(testing_data)
+
+# elapsed2 = time.clock()
+# elapsed2 = elapsed2 - start2
+
+# correctPredictionsKNN1 = 0
+# for item in testing_data:
+# 	#print "GUESS: " + item.probKNN1
+# 	#print "TRUTH: " + item.y
+# 	if item.probKNN1 == item.y:
+# 		correctPredictionsKNN1 += 1
+
+# print "RESULTS KNN - 1:"
+# print "Total cases: " + str(len(testing_data))
+# print "Correct Predictions: " + str(correctPredictionsKNN1)
+# print "Correctness percentage: " + str((correctPredictionsKNN1 / len(testing_data)) * 100) + "%"
+
+# correctPredictionsKNN3 = 0
+# for item in testing_data:
+# 	#print "GUESS: " + item.probKNN3
+# 	#print "TRUTH: " + item.y
+# 	if item.probKNN3 == item.y:
+# 		correctPredictionsKNN3 += 1
+
+# print "RESULTS KNN - 3:"
+# print "Total cases: " + str(len(testing_data))
+# print "Correct Predictions: " + str(correctPredictionsKNN3)
+# print "Correctness percentage: " + str((correctPredictionsKNN3 / len(testing_data)) * 100) + "%"
+
+# correctPredictionsKNN5 = 0
+# for item in testing_data:
+# 	#print "GUESS: " + item.probKNN5
+# 	#print "TRUTH: " + item.y
+# 	if item.probKNN5 == item.y:
+# 		correctPredictionsKNN5 += 1
+
+# print "RESULTS KNN - 5:"
+# print "Total cases: " + str(len(testing_data))
+# print "Correct Predictions: " + str(correctPredictionsKNN5)
+# print "Correctness percentage: " + str((correctPredictionsKNN5 / len(testing_data)) * 100) + "%"
+
+# print "Time spent in knn is: ", elapsed2
--- parser_csv.py
+++ parser_csv.py
@@ -8,6 +8,18 @@ from functools import partial
 FILE_NAME = 'bank-additional-full.csv'
 #FILE_NAME = 'bank.csv'
 
+class Common_Values:
+	
+	def __init__(self):
+		self.jobs = [0] * len(VALUES_JOB)
+		self.marital = [0] * len(VALUES_MARITAL)
+		self.education = [0] * len(VALUES_EDUCATION)
+		self.default = [0] * len(VALUES_DEFAULT)
+		self.housing = [0] * len(VALUES_HOUSING)
+		self.loan = [0] * len(VALUES_LOAN)
+		self.contact = [0] * len(VALUES_CONTACT)
+		self.poutcome = [0] * len(VALUES_POUTCOME)
+		
 def isFloat(s):
     try: 
         float(s)
@@ -55,6 +67,7 @@ def makeIntervals(min,max,qty):
 def createIntervalsAttributes():
 	first = True
 	second = True
+	common_values = Common_Values()
 	attributes = []
 	numericAtt = []
 	with open(FILE_NAME, 'rb') as csvfile:
@@ -77,7 +90,39 @@ def createIntervalsAttributes():
 				for a in c:
 					attribute = str.replace(a,'"','')
 					attribute = str.replace(attribute,'\n','')
-					attribute = str.replace(attribute,'\r','')					
+					attribute = str.replace(attribute,'\r','')		
+					if i == 1:
+					#attribute jobs
+						ind = VALUES_JOB.index(attribute)
+						common_values.jobs[ind] += 1
+					elif i == 2:
+					#attribute marital						
+						ind = VALUES_MARITAL.index(attribute)
+						common_values.marital[ind] += 1
+					elif i == 3:
+					#attribute education
+						ind = VALUES_EDUCATION.index(attribute)
+						common_values.education[ind] += 1
+					elif i == 4:
+					#attribute default
+						ind = VALUES_DEFAULT.index(attribute)
+						common_values.default[ind] += 1
+					elif i == 5:
+					#attribute housing
+						ind = VALUES_HOUSING.index(attribute)
+						common_values.housing[ind] += 1
+					elif i == 6:
+					#attribute loan
+						ind = VALUES_LOAN.index(attribute)
+						common_values.loan[ind] += 1
+					elif i == 7:
+					#attribute contact
+						ind = VALUES_CONTACT.index(attribute)
+						common_values.contact[ind] += 1
+					elif i == 14:
+					#attribute poutcome
+						ind = VALUES_POUTCOME.index(attribute)
+						common_values.poutcome[ind] += 1			
 					if isFloat(attribute):
 						if second:
 							numericAtt.append(attributes[i])
@@ -91,9 +136,9 @@ def createIntervalsAttributes():
 		max = filter(partial(is_not, None), max)
 		intervals = makeIntervals(min,max,4)
 		
-	return intervals,attributes
+	return intervals,attributes,common_values
 
-def getData(intervals,attributes):
+def getData(intervals,attributes,common_values, fix_unknown = False):
 	first = True
 	items = []
 	with open(FILE_NAME, 'rb') as csvfile:
@@ -108,36 +153,62 @@ def getData(intervals,attributes):
 				for a in c:
 					attribute_value = str.replace(a,'"','')
 					attribute_value = str.replace(attribute_value,'\n','')
-					attribute_value = str.replace(attribute_value,'\r','')						
+					attribute_value = str.replace(attribute_value,'\r','')	
+					if (fix_unknown) and (attribute_value == "unknown"):
+						if i == 1:
+						#attribute jobs
+							attribute_value = VALUES_JOB[common_values.jobs.index(max(common_values.jobs))]
+						elif i == 2:
+						#attribute marital						
+							attribute_value = VALUES_MARITAL[common_values.marital.index(max(common_values.marital))]
+						elif i == 3:
+						#attribute education
+							attribute_value = VALUES_EDUCATION[common_values.education.index(max(common_values.education))]
+						elif i == 4:
+						#attribute default
+							attribute_value = VALUES_DEFAULT[common_values.default.index(max(common_values.default))]
+						elif i == 5:
+						#attribute housing
+							attribute_value = VALUES_HOUSING[common_values.housing.index(max(common_values.housing))]
+						elif i == 6:
+						#attribute loan
+							attribute_value = VALUES_LOAN[common_values.loan.index(max(common_values.loan))]
+						elif i == 7:
+						#attribute contact
+							attribute_value = VALUES_CONTACT[common_values.contact.index(max(common_values.contact))]
+						elif i == 14:
+						#attribute poutcome
+							attribute_value = VALUES_POUTCOME[common_values.poutcome.index(max(common_values.poutcome))]
+					
 					if isFloat(attribute_value):
 						if i == 0:#es age
 							interval = intervals[0]
 							values = VALUES_AGE
-						elif i == 1:#es duration
+						elif i == 10:#es duration
 							interval = intervals[1]
 							values = VALUES_DURATION
-						elif i == 2:#es campaign
+						elif i == 11:#es campaign
 							interval = intervals[2]
 							values = VALUES_CAMPAIGN
-						elif i == 3:#es paydays
+						elif i == 12:#es paydays
 							interval = intervals[3]
 							values = VALUES_PDAYS
-						elif i == 4:#es previous
+						elif i == 13:#es previous
 							interval = intervals[4]
 							values = VALUES_PREVIOUS
-						elif i == 5:#es emp_var_rate
+						elif i == 14:#es emp_var_rate
 							interval = intervals[5]
 							values = VALUES_PREVIOUS
-						elif i == 6:#es cons_price_idx
+						elif i == 15:#es cons_price_idx
 							interval = intervals[6]
 							values = VALUES_PREVIOUS
-						elif i == 7:#es cons_conf_idx
+						elif i == 16:#es cons_conf_idx
 							interval = intervals[7]
 							values = VALUES_PREVIOUS
-						elif i == 8:#es euribor3m
+						elif i == 17:#es euribor3m
 							interval = intervals[8]
 							values = VALUES_PREVIOUS
-						elif i == 9:#es nr_employed
+						elif i == 18:#es nr_employed
 							interval = intervals[9]
 							values = VALUES_PREVIOUS
 										
@@ -149,7 +220,7 @@ def getData(intervals,attributes):
 							attribute_value = values[2]
 						elif float(attribute_value) >= interval[3][0]:
 							attribute_value = values[3]
-						i += 1
+					i += 1
 						
 					l.append(attribute_value)
 				item = Line(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9],l[10],l[11],l[12],l[13],l[14],l[15],l[16],l[17],l[18],l[19],l[20])
