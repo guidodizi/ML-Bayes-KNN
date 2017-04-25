4b3564953de2b9db68aa385cd7ae2d19b51dd22f
Nicolas <nicolascrocco@gmail.com>
2017-04-24 23:11:18 -0300

    fix para entrega

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ test_data = data[:index]
 #4/5
 train_data = data[index:] 
 
+#EN CASO DE QUERER CORRER EL ALGORITMO PARA UN CORPUS MAS CHICO DESCOMENTAR LO SIGUIENTE
+#tamanio = 100
+#train_data = train_data[:tamanio]
+#test_data = test_data[:tamanio]
 
 #PARTE B
 #validacion cruzada, divido mi train data en 10 subconjuntos
@@ -124,69 +128,67 @@ print "Correct Predictions: " + str(totalCorrectPredictionsKNN5)
 print "Correctness percentage: " + str((totalCorrectPredictionsKNN5 / len(train_data)) * 100) + "%"
 print "------------------------------------------"
 
-## PARTE C!!!!!
-# index_testing = 10
-# testing_data = test_data
-# print len(testing_data)
-# bayesian = BayesianStruct(train_data)
+# PARTE C!!!!!
+testing_data = test_data
+bayesian = BayesianStruct(train_data)
 
-# bayesian.processData()
+bayesian.processData()
 
-# bayesian.evalData(testing_data)
+bayesian.evalData(testing_data)
 
-# correctPredictionsBayesian = 0
-# for item in testing_data:
-# 	#print "GUESS: " + item.probBayesian
-# 	#print "TRUTH: " + item.y
-# 	if item.probBayesian == item.y:
-# 		correctPredictionsBayesian += 1
-# print "RESULTS BAYESIAN LEARNING:"
-# print "Total cases: " + str(len(testing_data))
-# print "Correct Predictions: " + str(correctPredictionsBayesian)
-# print "Correctness percentage: " + str((correctPredictionsBayesian / len(testing_data)) * 100) + "%"
+correctPredictionsBayesian = 0
+for item in testing_data:
+	#print "GUESS: " + item.probBayesian
+	#print "TRUTH: " + item.y
+	if item.probBayesian == item.y:
+		correctPredictionsBayesian += 1
+print "RESULTS BAYESIAN LEARNING:"
+print "Total cases: " + str(len(testing_data))
+print "Correct Predictions: " + str(correctPredictionsBayesian)
+print "Correctness percentage: " + str((correctPredictionsBayesian / len(testing_data)) * 100) + "%"
 
-# start2 = time.clock() 
+start2 = time.clock() 
 
-# knn = KNN(5, train_data)
-# knn.evalData(testing_data)
+knn = KNN(5, train_data)
+knn.evalData(testing_data)
 
-# elapsed2 = time.clock()
-# elapsed2 = elapsed2 - start2
+elapsed2 = time.clock()
+elapsed2 = elapsed2 - start2
 
-# correctPredictionsKNN1 = 0
-# for item in testing_data:
-# 	#print "GUESS: " + item.probKNN1
-# 	#print "TRUTH: " + item.y
-# 	if item.probKNN1 == item.y:
-# 		correctPredictionsKNN1 += 1
+correctPredictionsKNN1 = 0
+for item in testing_data:
+	#print "GUESS: " + item.probKNN1
+	#print "TRUTH: " + item.y
+	if item.probKNN1 == item.y:
+		correctPredictionsKNN1 += 1
 
-# print "RESULTS KNN - 1:"
-# print "Total cases: " + str(len(testing_data))
-# print "Correct Predictions: " + str(correctPredictionsKNN1)
-# print "Correctness percentage: " + str((correctPredictionsKNN1 / len(testing_data)) * 100) + "%"
+print "RESULTS KNN - 1:"
+print "Total cases: " + str(len(testing_data))
+print "Correct Predictions: " + str(correctPredictionsKNN1)
+print "Correctness percentage: " + str((correctPredictionsKNN1 / len(testing_data)) * 100) + "%"
 
-# correctPredictionsKNN3 = 0
-# for item in testing_data:
-# 	#print "GUESS: " + item.probKNN3
-# 	#print "TRUTH: " + item.y
-# 	if item.probKNN3 == item.y:
-# 		correctPredictionsKNN3 += 1
+correctPredictionsKNN3 = 0
+for item in testing_data:
+	#print "GUESS: " + item.probKNN3
+	#print "TRUTH: " + item.y
+	if item.probKNN3 == item.y:
+		correctPredictionsKNN3 += 1
 
-# print "RESULTS KNN - 3:"
-# print "Total cases: " + str(len(testing_data))
-# print "Correct Predictions: " + str(correctPredictionsKNN3)
-# print "Correctness percentage: " + str((correctPredictionsKNN3 / len(testing_data)) * 100) + "%"
+print "RESULTS KNN - 3:"
+print "Total cases: " + str(len(testing_data))
+print "Correct Predictions: " + str(correctPredictionsKNN3)
+print "Correctness percentage: " + str((correctPredictionsKNN3 / len(testing_data)) * 100) + "%"
 
-# correctPredictionsKNN5 = 0
-# for item in testing_data:
-# 	#print "GUESS: " + item.probKNN5
-# 	#print "TRUTH: " + item.y
-# 	if item.probKNN5 == item.y:
-# 		correctPredictionsKNN5 += 1
+correctPredictionsKNN5 = 0
+for item in testing_data:
+	#print "GUESS: " + item.probKNN5
+	#print "TRUTH: " + item.y
+	if item.probKNN5 == item.y:
+		correctPredictionsKNN5 += 1
 
-# print "RESULTS KNN - 5:"
-# print "Total cases: " + str(len(testing_data))
-# print "Correct Predictions: " + str(correctPredictionsKNN5)
-# print "Correctness percentage: " + str((correctPredictionsKNN5 / len(testing_data)) * 100) + "%"
+print "RESULTS KNN - 5:"
+print "Total cases: " + str(len(testing_data))
+print "Correct Predictions: " + str(correctPredictionsKNN5)
+print "Correctness percentage: " + str((correctPredictionsKNN5 / len(testing_data)) * 100) + "%"
 
-# print "Time spent in knn is: ", elapsed2
+print "Time spent in knn is: ", elapsed2
